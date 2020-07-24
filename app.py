7e89435857e5c92ba05be244514c0ca6d076125b
sven <sven@so36.net>
2020-07-24 11:50:53 +0200

    chore(code): format with black

--- app.py
+++ app.py
@@ -10,27 +10,28 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 planets = [
-    {"name": "Mercury", "position": 1, "moons":0},
-    {"name": "Venus", "position": 2, "moons":0},
-    {"name": "Earth", "position": 3, "moons":1},
-    {"name": "Mars", "position": 4, "moons":2},
-    {"name": "Jupiter", "position": 5, "moons":79},
-    {"name": "Saturn", "position": 6, "moons":62},
-    {"name": "Uranus", "position": 7, "moons":27},
-    {"name": "Neptune", "position": 8, "moons":14},
+    {"name": "Mercury", "position": 1, "moons": 0},
+    {"name": "Venus", "position": 2, "moons": 0},
+    {"name": "Earth", "position": 3, "moons": 1},
+    {"name": "Mars", "position": 4, "moons": 2},
+    {"name": "Jupiter", "position": 5, "moons": 79},
+    {"name": "Saturn", "position": 6, "moons": 62},
+    {"name": "Uranus", "position": 7, "moons": 27},
+    {"name": "Neptune", "position": 8, "moons": 14},
 ]
 
 
-@app.route('/planets/<id>')
+@app.route("/planets/<id>")
 def one_planet(id):
-    return json.dumps(planets[int(id)-1]), 200, {'Content-Type': 'application/json'}
+    return json.dumps(planets[int(id) - 1]), 200, {"Content-Type": "application/json"}
 
-@app.route('/planets')
+
+@app.route("/planets")
 def list_planets():
-    return json.dumps(planets), 200, {'Content-Type': 'application/json'}
+    return json.dumps(planets), 200, {"Content-Type": "application/json"}
 
 
-@app.route('/webhook', methods=['GET','POST'])
+@app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     logging.info("Request received")
     logging.info(request.args)
